54a8a72b9a34e66a063455c04a9a0c0f2703d432
Arunkumar Gopalan <96195521+arun13go@users.noreply.github.com>
2023-01-26 16:10:21 +0000

    Update and rename Test-ComposeModel-ELMONB.py to Test-ComposeModel-KeyValue.py

--- FormRecognizer/Test-ComposeModel-KeyValue.py
+++ FormRecognizer/Test-ComposeModel-KeyValue.py
@@ -15,6 +15,9 @@ secure methods to store and access your credentials. For more information, see
 https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
 """
 
+# This code will call Form Recognizer Compose model and return restult as Key Value repsonse
+
+
 endpoint = "YOUR END POINT URL HERE"
 key = "YOUR END POINT KEY HERE"
 
